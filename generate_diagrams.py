e0a4467b34beef73c8cc08516706d49a9344a072
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-11-02 22:08:51 +0000

    changes

--- generate_diagrams.py
+++ generate_diagrams.py
@@ -1,16 +1,16 @@
 from zines import find, to_tikz
 
 for i, dims in enumerate([(2, 2), (4, 2), (4, 4), (8, 4), (8, 8)]):
-    print("Starting", i)
-    fname = f"output/routes{i+1}.tex"
+    assert dims[0] * dims[1] == 2 ** (i + 2)
+    print("Starting", i + 2)
+    fname = f"output/routes{i+2}.tex"
     with open(fname, "w") as f:
         f.write("\\documentclass{standalone}\n")
         f.write("\\usepackage{tikz}\n")
         f.write("\\begin{document}\n")
-    assert dims[0] * dims[1] == 2 ** (i + 2)
     for path in find(*dims):
         with open(fname, "a") as f:
             f.write(to_tikz(path))
     with open(fname, "a") as f:
         f.write("\\end{document}\n")
-    print("Finished", i)
+    print("Finished", i + 2)
--- sequence.py
+++ sequence.py
@@ -2,5 +2,4 @@ from zines import find
 
 for i, dims in enumerate([(2, 2), (4, 2), (4, 4), (8, 4), (8, 8), (16, 8)]):
     assert dims[0] * dims[1] == 2 ** (i + 2)
-    res = find(*dims)
-    print(i + 2, len(res))
+    print(i + 2, len(find(*dims)))
